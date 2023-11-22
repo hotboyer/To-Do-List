b98cc08c76928d81028359c0d9c3c83b671c37da
Tobi <babalolatobiloba4real@gmail.com>
2023-11-22 11:45:57 +0200

    updated new changes to index.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const path = require ('path');
+const path = require ("path");
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "views"));
-
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
